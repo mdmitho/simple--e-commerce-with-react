c4b077622ab5aea287c3dc55578e8c080ec0ac75
Hossen <mdmitho199911@gmail.com>
2022-03-21 22:26:00 +0600

    load data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './components/Header/Header';
+import Shop from './components/Header/Shop/Shop';
 
 
 function App() {
   return (
     <div className="">
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
