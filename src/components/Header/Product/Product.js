4f179ebe8ce93c4317a41e0c6c0fbf220cec19ae
Hossen <mdmitho199911@gmail.com>
2022-03-21 23:06:17 +0600

    display product img and name

---
+++ src/components/Header/Product/Product.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props)
+    const {name, img, seller, price, ratings} = props.product;
+ 
+    return (
+        <div className='product'>
+           
+            <img src={img} alt="" />
+            <p className='product-name'>{name}</p>
+
+        </div>
+    );
+};
+
+export default Product;
