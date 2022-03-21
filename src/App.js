6c6830d10df7fecdebef059c0dc9952442e14e61
Hossen <mdmitho199911@gmail.com>
2022-03-21 22:00:32 +0600

    header , logo , menu

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
+    <div className="">
       <Header></Header>
     </div>
   );
