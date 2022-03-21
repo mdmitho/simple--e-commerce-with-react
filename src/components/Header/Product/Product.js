34ba41e4063d2973864eca8efd0c66e2522a1111
Hossen <mdmitho199911@gmail.com>
2022-03-22 00:11:02 +0600

    style product cart

--- src/components/Header/Product/Product.js
+++ src/components/Header/Product/Product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Product.css'
-
+ 
 const Product = (props) => {
     console.log(props)
     const {name, img, seller, price, ratings} = props.product;
@@ -9,9 +9,15 @@ const Product = (props) => {
         <div className='product'>
            
             <img src={img} alt="" />
+            <div className="product-info">
             <p className='product-name'>{name}</p>
+            <p>Price : ${price}</p>
+            <p><small>Seller : {seller}</small></p>
+            <p><small>Ratings : {ratings} stars</small></p>
+</div>
+<button className='btn-cart'>Add to Cart</button>
+</div>
 
-        </div>
     );
 };
 
